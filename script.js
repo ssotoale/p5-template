aa3ba51ca3a2f067565445356c9f4622658ed0cd
Samantha S. Ale <52551359+ssotoale@users.noreply.github.com>
2021-06-24 11:36:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -23,14 +23,4 @@ function draw() {
   let rate = 10;
   const mooX = 410;
   const mooY = 200;
-  let distanceX, distanceY, distance;
-  distanceX = mouseX - mooX;
-  distanceY = mouseY - mooY;
-  distance = Math.pow(Math.pow(distanceX,2) + Math.pow(distanceY,2),0.5);
-  textSize(10+6*Math.log(distance/50));
-  if(frameCount%rate == 0) {
-  fill(random(colorlist));
-    let currWord = random(phraselist);
-    text(currWord,mouseX,mouseY);
-  }
 }
