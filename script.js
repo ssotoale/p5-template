9f588babb2ec7f6f775e11c34aba44ff754e288c
Samantha S. Ale <52551359+ssotoale@users.noreply.github.com>
2021-06-24 10:33:30 -0700

    Update script.js
    manually copy and pasted code bc i cant git

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+let backgroundimg;
+let words;
+let phraselist = [];
+
+
+function preload() {
+  words = loadStrings('./words.txt');
+  backgroundimg = loadImage('https://wallpaperaccess.com/full/1862683.jpg');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(255);
+  background(255);
+  words.forEach((phrase) => { 
+    phraselist.push(phrase)});
+  image(backgroundimg,0,0);
+  console.log(phraselist);
+  noStroke();
 }
 
 function draw() {
-  noStroke();
+  let rate = 10;
+  const mooX = 410;
+  const mooY = 200;
+  let distanceX, distanceY, distance;
+  distanceX = mouseX - mooX;
+  distanceY = mouseY - mooY;
+  distance = Math.pow(Math.pow(distanceX,2) + Math.pow(distanceY,2),0.5);
+  textSize(10+6*Math.log(distance/50));
+  if(frameCount%rate == 0) {
   fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+    let currWord = random(phraselist);
+    text(currWord,mouseX,mouseY);
+  }
 }
